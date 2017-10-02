15a52dbbd7d663c8b791862da9f2d50e08a0ade0
sfo <sfragua83@gmail.com>
2017-10-02 02:03:23 +0200

    pipe testing

---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { DbhcardsPipe } from './../pipes/dbhcards/dbhcards';
+@NgModule({
+	declarations: [DbhcardsPipe],
+	imports: [],
+	exports: [DbhcardsPipe]
+})
+export class PipesModule {}
