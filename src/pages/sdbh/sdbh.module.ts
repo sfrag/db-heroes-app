f1203f180a463922f705d45da8265235ca4c7ed1
sfo <sfragua83@gmail.com>
2017-11-04 03:36:07 +0100

    adding pages again

---
+++ src/pages/sdbh/sdbh.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SdbhPage } from './sdbh';
+
+@NgModule({
+  declarations: [
+    SdbhPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SdbhPage),
+  ],
+})
+export class SdbhPageModule {}
---
+++ src/pages/sdbh1/sdbh1.ts
@@ -0,0 +1,108 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { FirebaseDbProvider } from '../../providers/firebase-db/firebase-db';
+import 'rxjs/add/operator/first';
+
+/**
+ * Generated class for the Sdbh1Page page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-sdbh1',
+  templateUrl: 'sdbh1.html',
+})
+export class Sdbh1Page {
+
+  cards: any;
+  ucards: any;
+  processedcards: any;
+  ucardscount: any;
+  counter: number;
+  newer: boolean;
+  subscription: any;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private dbhDb: FirebaseDbProvider ) {
+      this.counter = 1;
+  }
+
+  savecard(card){
+    
+    this.newer = true;
+    let user_card = this.ucards;
+
+    // Si ya la tenemos, solo actualizaremos el valor de counter
+    
+    for(let i = 0;i<user_card.length; i++){
+      let test = user_card[i];
+      if(test.id == card.id){
+        this.newer = false;
+        this.updatecounter(test);
+        return;
+      }
+    }
+
+    // Si la carta que queremos añadir es nueva, tenemos que añadir toda la carta a la base de datos de cartas de este usuario
+    
+    if(this.newer){
+      let newcard = {}
+      newcard = {
+        collection: card.collection,
+        id: card.id,
+        counter: this.counter
+      }
+      this.dbhDb.saveCard(newcard);
+    }
+  }
+
+  updatecounter(card){
+    if(this.ucards != undefined){
+          this.ucardscount = card.counter;
+          this.ucardscount ++;
+          this.dbhDb.countCards(card.id,this.ucardscount);
+      }
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Sdbh1Page');
+
+    this.dbhDb.getCards().first().subscribe(cards=>{  
+      this.subscription = this.dbhDb.getUserCards().subscribe(ucards=>{
+        
+        this.ucards = ucards;
+        this.cards = cards[1].cards;
+        this.processedcards = cards;
+
+        for(let i=0; i<this.ucards.length; i++){
+          var pilla = this.ucards[i];
+          for( let j=0; j<this.processedcards.length; j++){
+            var pilla2 = this.processedcards[j];
+            if(pilla.collection == pilla2.name){
+              console.log("Es de esta colección, busco dentro");
+              for(let k=0; k<pilla2.cards.length; k++){
+                var pilla3 = pilla2.cards[k];
+                if(pilla3.id == pilla.id){
+                  pilla3.counter = pilla.counter;
+                }
+              }
+            }
+          }
+        }
+      });
+    });
+    
+  }
+
+  ngOnDestroy(){
+    if(this.subscription != undefined){
+      this.subscription.unsubscribe();
+    }
+  }
+}
