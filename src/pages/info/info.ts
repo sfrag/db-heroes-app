8d76c026172915c2d47b114d290092723fbc3e2c
sfo <sfragua83@gmail.com>
2017-12-27 22:44:10 +0100

    info page update

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AuthProvider } from '../../providers/auth/auth';
 
 import { FirebaseDbProvider } from '../../providers/firebase-db/firebase-db';
 import 'rxjs/add/operator/first';
