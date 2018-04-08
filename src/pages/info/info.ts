cf30c9f66d73d050893fd85eca898adf2f2d6ccf
sfo <sfragua83@gmail.com>
2018-04-08 20:35:32 +0200

    Added collections info page

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -17,27 +17,60 @@ import 'rxjs/add/operator/first';
 })
 export class InfoPage {
 
+  public collections: any;
+  numberofCollections: number;
+  cards: any;
+  ucards: any;
+  totalCards: Array<Object>;
+  userCollectionCards: number;
+  collectionProcessed: Object;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private dbhDb: FirebaseDbProvider
   ) {
+    this.totalCards = [];
   }
-  
-  collections: any;
-  numberofCollections: number;
-  cards: any;
-  totalCards: number;
 
   ionViewDidLoad() {
+
+    //get collections
     this.dbhDb.getCollections().first().subscribe(collections=>{
       this.collections = collections;
       this.numberofCollections = this.collections.length;
-      //info page collections
+    
+
+      this.dbhDb.getUserCards().first().subscribe(ucards=>{
+        this.ucards = ucards;
+
+        for(let i=0; i < this.collections.length; i++){
+          
+          this.userCollectionCards = 0;
+    
+          for(let j=0; j < this.ucards.length; j++){
+    
+            if(ucards[j].collection){
+    
+              if(ucards[j].collection == this.collections[i].id.toUpperCase())
+              {
+                this.userCollectionCards++;
+              }
+            }
+          }
+
+          this.collectionProcessed = {
+            collection_id: this.collections[i].id,
+            collection_total_cards: this.collections[i].totalcards,
+            collection_have_cards: this.userCollectionCards,
+            collection_no_have_cards: this.collections[i].totalcards - this.userCollectionCards
+          }
+          
+          this.totalCards.push(this.collectionProcessed);
+        }
+      });
     });
 
-    this.dbhDb.getCards().first().subscribe(cards=>{
-      this.cards = cards;
-    });
   }
+
 }
