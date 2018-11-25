f9a9ca6807bb5178f2b3ff4e0468c8c604940f88
sfo <sfragua83@gmail.com>
2018-11-25 01:48:21 +0100

    Adding new universe mission 5 collection

---
+++ src/pages/collections/univmis5/univmis5.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { Univmis5Page } from "./univmis5";
+
+@NgModule({
+  declarations: [Univmis5Page],
+  imports: [IonicPageModule.forChild(Univmis5Page)]
+})
+export class Univmis5PageModule {}
